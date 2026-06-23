dcb6cac09b692c0c7472c4508d2844856db46b96
techyminati <techyminati@outlook.com>
2026-06-23 11:47:28 +0530

    config: Enable background blur for widget picker
    Change-Id: Ice370f166a73d395e223a8892a6f3350ce94106d
    Signed-off-by: techyminati <techyminati@outlook.com>

--- config/common.mk
+++ config/common.mk
@@ -10,6 +10,10 @@ PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 
 PRODUCT_BRAND ?= haloUI
 
+# Enable background blur for widget picker
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.launcher.depth.widget=true
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
